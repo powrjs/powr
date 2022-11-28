a683ef50f6ead3aa33fc84a318b1e75ad17cb9ee
Max Power <alex@alexaa.dev>
2022-11-28 06:19:55 -0300

    chore(parser): adds `pest`

--- pkg/parser/src/lib.rs
+++ pkg/parser/src/lib.rs
@@ -1,8 +1,16 @@
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "javascript.pest"]
+struct JavaScriptParser;
+
 #[cfg(test)]
 mod tests {
+    use crate::{JavaScriptParser, Rule};
+    use pest::Parser;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        let js = JavaScriptParser::parse(Rule::field, "123").unwrap();
     }
 }
