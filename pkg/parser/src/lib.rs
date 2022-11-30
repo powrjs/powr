b3aff356b4d0cdcbb60fe924aaa3b3467cc0321f
Max Power <alex@alexaa.dev>
2022-11-30 02:00:15 -0300

    test(parser): adds tests for values

--- pkg/parser/src/lib.rs
+++ pkg/parser/src/lib.rs
@@ -11,6 +11,35 @@ mod tests {
 
     #[test]
     fn it_works() {
-        JavaScriptParser::parse(Rule::program, "if (a > b) { return 3 }").unwrap();
+        JavaScriptParser::parse(Rule::program, "1 + 1;").unwrap();
+    }
+
+    #[test]
+    fn booleans() {
+        JavaScriptParser::parse(Rule::program, "true;").unwrap();
+        JavaScriptParser::parse(Rule::program, "false;").unwrap();
+    }
+
+    #[test]
+    fn boolean_checking() {
+        JavaScriptParser::parse(Rule::program, "true === true;").unwrap();
+        JavaScriptParser::parse(Rule::program, "true === false;").unwrap();
+        JavaScriptParser::parse(Rule::program, "false === true;").unwrap();
+        JavaScriptParser::parse(Rule::program, "false === false;").unwrap();
+    }
+
+    #[test]
+    fn scientific_notation() {
+        JavaScriptParser::parse(Rule::program, "1e-1;").unwrap();
+    }
+
+    #[test]
+    fn arrays() {
+        JavaScriptParser::parse(Rule::program, "[1, 2, 3];").unwrap();
+    }
+
+    #[test]
+    fn objects() {
+        JavaScriptParser::parse(Rule::program, "({a: 1, b: 2, c: 3});").unwrap();
     }
 }
