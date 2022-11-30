26e4c1a1043a49d82aa1bbc2c081a650488ace4a
Max Power <alex@alexaa.dev>
2022-11-30 01:40:52 -0300

    fix(parser): changes `main` to `grammar.pest` too

--- pkg/parser/src/main.rs
+++ pkg/parser/src/main.rs
@@ -3,12 +3,12 @@ use pest_derive::Parser;
 use std::fs::read;
 
 #[derive(Parser)]
-#[grammar = "javascript.pest"]
+#[grammar = "grammar.pest"]
 struct JavaScriptParser;
 
 fn main() {
     let code = String::from_utf8(read("examples/values.js").unwrap()).unwrap();
-    match JavaScriptParser::parse(Rule::program, code.as_str()) {
+    match JavaScriptParser::parse(Rule::program, &code) {
         Ok(js) => println!("{:#?}", js),
         Err(e) => eprintln!("{}", e),
     };
