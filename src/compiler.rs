2f4bb317212752e6c58b97954464818b57a3092a
Max Power <alex@alexaa.dev>
2022-12-03 00:45:29 -0300

    feat(comp): creates `CompilerError`

--- src/compiler.rs
+++ src/compiler.rs
@@ -3,6 +3,19 @@ use inkwell::context::Context;
 use inkwell::module::Module;
 use inkwell::values::{FunctionValue, PointerValue};
 use std::collections::HashMap;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter};
+
+#[derive(Debug)]
+pub struct CompilerError;
+
+impl Display for CompilerError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "An error occurred while compiling")
+    }
+}
+
+impl Error for CompilerError {}
 
 pub struct Compiler<'a, 'ctx> {
     pub context: &'a Context,
