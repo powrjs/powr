5bccf751d0b67e0cf5cbd7428a3422eb78f7102f
Max Power <alex@alexaa.dev>
2022-12-03 03:28:25 -0300

    fix(scripts/compile): wrong project name

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cargo run --bin powr-compiler "const a = 1"
+cargo run --bin powr "const a = 1"
 # opt ./compiled.ll
 llc -filetype=obj compiled.ll -o compiled.o
 clang compiled.o -o compiled
